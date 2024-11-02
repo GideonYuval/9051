47cf821873fea2a396e158cff808043914898439
GideonYuval <72265313+giladyuval@users.noreply.github.com>
2024-11-02 16:29:40 +0200

    fixed based on inputs from teachers

--- 9051/Program.cs
+++ 9051/Program.cs
@@ -34,10 +34,10 @@ namespace _9051
         private static int Sod1H(int x, int y)
         {
             if (x == 0) return 0;
-            int digit = x % 10;
-            int sum = 0;
-            if (y % 2 != 0) sum = digit;
-            return sum + Sod1H(x / 10, y + 1);
+            int d = x % 10;
+            int s = 0;
+            if (y % 2 != 0) s = d;
+            return s + Sod1H(x / 10, y + 1);
         }
 
         public static int Sod2(int x)
@@ -48,16 +48,16 @@ namespace _9051
         private static int Sod2H(int x, int y)
         {
             if (x == 0) return 0;
-            int digit = x % 10;
-            int sum = 0;
-            if (y % 2 == 0) sum = digit;
-            return sum + Sod2H(x / 10, y + 1);
+            int d = x % 10;
+            int s = 0;
+            if (y % 2 == 0) s = d;
+            return s + Sod2H(x / 10, y + 1);
         }
 
         public static bool Sod3(int x)
         {
-            int sum = Sod1(x) + Sod2(x);
-            return sum % 3 == 0;
+            int s = Sod1(x) + Sod2(x);
+            return s % 3 == 0;
         }
 
 
@@ -91,21 +91,21 @@ namespace _9051
         static int What1(int x)
         {
             if (x == 0) return 0;
-            int lastDigit = x % 10;
-            int sum = What1(x / 10);
-            if (lastDigit % 2 == 0)
-                return sum + lastDigit;
-            return sum;
+            int ld = x % 10;
+            int s = What1(x / 10);
+            if (ld % 2 == 0)
+                return s + ld;
+            return s;
         }
 
         static int What2(int x)
         {
             if (x == 0) return 0;
-            int lastDigit = x % 10;
-            int sum = What2(x / 10);
-            if (lastDigit % 2 != 0)
-                return sum + lastDigit;
-            return sum;
+            int ld = x % 10;
+            int s = What2(x / 10);
+            if (ld % 2 != 0)
+                return s + ld;
+            return s;
         }
 
         static bool What3(int x)
@@ -113,9 +113,8 @@ namespace _9051
             int x1 = What1(x);
             int x2 = What2(x);
             int d = Math.Abs(x1 - x2);
-            if (d < 0) return false;
-            double root = Math.Sqrt(d);
-            return root * root == d;
+            double r = Math.Sqrt(d);
+            return r * r == d;
         }
 
 
